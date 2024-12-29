f15ccc0f077c7c09a44ea08e56caa6a06fced719
rsshakil <shakilreyan9397@gmail.com>
2024-12-29 18:21:29 +0900

    updated

--- phalco/vite.config.js
+++ phalco/vite.config.js
@@ -5,16 +5,6 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import * as path from "node:path";
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        proxy: {
-            '/@vite/client': {
-                target: 'http://52.53.159.110:5173',
-                changeOrigin: true,  // To avoid CORS issues
-            },
-        },
-      },
     plugins: [
         laravel({
             input: ['resources/js/app.js'],
